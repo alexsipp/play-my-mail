52b3b8984cef344008489c8c7cf8c71d0df26099
MartinAnd <andres.martinlopez@gmail.com>
2013-10-05 10:15:08 +0100

    First commit

--- routes/pages.js
+++ routes/pages.js
@@ -8,6 +8,22 @@ app.get('/', function (req, res) {
 // Home Page
 app.get('/home', function (req, res) {
 
+	app.get('playmy').delete_previous_files(function(err){
+		app.get('playmy').get_email_subjects(, , "INBOX", 1000, function(error, titles){
+
+			console.log('GOT TITLES');
+			
+			console.log('creating_file');
+			app.get('playmy').write_file("input_file.txt",titles,function(err){
+				console.log('DONE WITH FILE');
+
+				app.get('playmy').calculate_word_freq('freq_file.txt', function(err){
+					console.log('processed word frequency');
+				});
+			});
+		});
+	});
+
 	res.render('home', {
 	    title: 'Home',
 	    page_nav: 'home'
