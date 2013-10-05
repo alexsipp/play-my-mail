2afebbf832905dd4c8e18146e2f4b5e2527c48c8
Chris <chris.a.blakey@googlemail.com>
2013-10-05 11:21:48 +0100

    Changed layout and added form

--- routes/pages.js
+++ routes/pages.js
@@ -8,25 +8,37 @@ app.get('/', function (req, res) {
 // Home Page
 app.get('/home', function (req, res) {
 
-	app.get('playmy').delete_previous_files(function(err){
-		app.get('playmy').get_email_subjects(, , "INBOX", 1000, function(error, titles){
-
-			console.log('GOT TITLES');
-			
-			console.log('creating_file');
-			app.get('playmy').write_file("input_file.txt",titles,function(err){
-				console.log('DONE WITH FILE');
-
-				app.get('playmy').calculate_word_freq('freq_file.txt', function(err){
-					console.log('processed word frequency');
-				});
-			});
-		});
-	});
-
 	res.render('home', {
 	    title: 'Home',
 	    page_nav: 'home'
 	});    
 	
 });
+
+app.post('/login', function (req, res) {
+	var email = req.body.username;
+	var password = req.body.password;
+
+	if (username || password) {
+		app.get('playmy').delete_previous_files(function(err){
+			app.get('playmy').get_email_subjects(email, password, "INBOX", 1000, function(error, titles){
+
+				console.log('GOT TITLES');
+				
+				console.log('creating_file');
+				app.get('playmy').write_file("input_file.txt",titles,function(err){
+					console.log('DONE WITH FILE');
+
+					app.get('playmy').calculate_word_freq('freq_file.txt', function(err){
+						console.log('processed word frequency');
+					});
+				});
+			});
+		});	
+	}
+	else {
+		res.redirect('/home');
+	}
+	
+
+})
